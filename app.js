9dc2d87f345ea8a04d03a98e9a3a6efe0939de4c
Binshadh Basheer <binshadh_b210517cs@nitc.ac.in>
2023-06-10 13:46:30 +0530

    done

--- app.js
+++ app.js
@@ -3,26 +3,39 @@ const app = express();
 app.set("view engine", "ejs");
 // Listen to port 3000
 app.listen(3000);
- // Route for home page
+
 app.get("/", (req, res) => {
-  res.render("index",{title:'Home'});   
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
- // Route for about page
+// Route for about page
 app.get("/about", (req, res) => {
   // Display about.html
-  res.render("about",{title:' about'});   
- 
+  res.render("about", { title: " about" });
 });
- // Redirect /about-us to /about
+// Redirect /about-us to /about
 app.get("/about-us", (req, res) => {
-  res.redirect("about",{title:'about'});
+  res.redirect("about", { title: "about" });
 });
 
-app.get('/blogs/create',(re,res)=>{
-  res.render('create',{title:'create'});
+app.get("/blogs/create", (re, res) => {
+  res.render("create", { title: "create" });
 });
- // 404 error page
+// 404 error page
 app.use((req, res) => {
   // Display 404.html
-  res.status(404).render("404",{title:'404'}); 
+  res.status(404).render("404", { title: "404" });
 });
